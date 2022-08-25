4df984b995c4693bb413a4cb2e95f0187671beab
Cody <cody.tian@icloud.com>
2022-08-25 15:36:03 -0700

    add slideRightTests() and slideRight()

--- gameBoard.js
+++ gameBoard.js
@@ -27,7 +27,15 @@ class GameBoard {
         this.score = 0     // the current score. increases on each combine
     }
 
-    slideRight() {
+    // slideRight() {
+
+    // }
+
+    combineRight() {
+
+    }
+
+    moveRight() {
 
     }
 
@@ -37,6 +45,57 @@ class GameBoard {
 
     spawnRandomFour() {
 
+    }
+
+    // preforms the right move command on a single row, except without the combine part
+    slideRight(row){
+        let result = [...row]
+
+        for (let cellIndex = 3; cellIndex >= 0; cellIndex--) {
+            let cell = result[cellIndex]
+
+            for (let possibleSlideIndex = cellIndex; possibleSlideIndex < 4; possibleSlideIndex++) {
+                result[cellIndex] = 0
+
+                result[possibleSlideIndex] = cell
+            }
+        }
+
+        return result
+    }
+
+
+    // runs tests for the function slideRight()
+    runSlideRightTests() {
+        const testTuples = [
+            // basic single cell slides
+            {'arr': [0, 0, 0, 0], 'ans': [0, 0, 0, 0]},
+            {'arr': [0, 0, 0, 2], 'ans': [0, 0, 0, 2]},
+            {'arr': [0, 0, 2, 0], 'ans': [0, 0, 0, 2]},
+            {'arr': [0, 2, 0, 0], 'ans': [0, 0, 0, 2]},
+            {'arr': [2, 0, 0, 0], 'ans': [0, 0, 0, 2]},
+            // slides with arr[3] filled
+            {'arr': [0, 2, 0, 2], 'ans': [0, 0, 2, 2]},
+            {'arr': [2, 0, 0, 2], 'ans': [0, 0, 2, 2]},
+            // test mid-array multi-slide
+            {'arr': [2, 0, 2, 0], 'ans': [0, 0, 2, 2]},
+            {'arr': [2, 2, 0, 0], 'ans': [0, 0, 2, 2]},
+            {'arr': [2, 2, 2, 0], 'ans': [0, 2, 2, 2]},
+            // no slides
+            {'arr': [2, 2, 2, 2], 'ans': [2, 2, 2, 2]}
+        ]
+
+        console.log(testTuples[1])
+
+        for (const i in testTuples) {
+            const test = testTuples[i]
+
+            const testCase = test['arr']
+            const expectedResult = test['ans']
+            const slideResult = this.slideRight(testCase)
+
+            console.log(`${i.padStart(2, '0')}.slide→[${testCase}]→[${slideResult}] ?= [${expectedResult}]`)
+        }
     }
 }
 
